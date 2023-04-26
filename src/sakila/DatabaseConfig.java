9ccc37b8b7c2501496589c9b3dafb3baab1f0225
vafdaf12 <vafdaf11@gmail.com>
2023-04-26 15:18:49 +0200

    feat: Load database information from environment variables

---
+++ src/sakila/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package sakila;
+
+public class DatabaseConfig {
+    private final String proto, host, port, database;
+    private final String username, password;
+
+    static DatabaseConfig load() throws IllegalStateException {
+        return new DatabaseConfig();
+    }
+
+    private DatabaseConfig() throws IllegalStateException {
+        proto = requireVariable("dvdrental_DB_PROTO");
+        host = requireVariable("dvdrental_DB_HOST");
+        database = requireVariable("dvdrental_DB_NAME");
+        port = System.getenv("dvdrental_DB_PORT");
+
+        username = requireVariable("dvdrental_DB_USERNAME");
+        password = requireVariable("dvdrental_DB_PASSWORD");
+    }
+
+    private static String requireVariable(String name) throws IllegalStateException {
+        String value = System.getenv(name);
+        if(value == null) throw new IllegalStateException("Missing environment variable: \"" + name + "\"");
+        return value;
+    }
+
+
+    public String getConnectionString() {
+        String connection = proto + "://" + host;
+        if(port != null) connection += ":" + port;
+        connection += "/" + database;
+        return connection;
+    }
+
+    public String getPassword() { return password; }
+    public String getUsername() { return username; }
+}
