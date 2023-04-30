2bff78f853104db2bf81f60376914119682cfbec
vafdaf12 <vafdaf11@gmail.com>
2023-04-30 13:45:26 +0200

    feat: Add functionality to fuzzy-search by name and activity

--- src/sakila/model/StaffModel.java
+++ src/sakila/model/StaffModel.java
@@ -8,6 +8,9 @@ import java.sql.SQLException;
 public class StaffModel {
     private final PreparedStatement selectAll;
 
+    private String searchName = null;
+    private Boolean searchActive = null;
+
     public StaffModel() throws SQLException {
         Connection connection = Database.getConnection();
         selectAll = connection.prepareStatement("""
@@ -24,12 +27,28 @@ public class StaffModel {
             FROM staff s
                 INNER JOIN address a ON s.address_id = a.address_id
                 INNER JOIN city ci ON a.city_id = ci.city_id
-                INNER JOIN country co ON co.country_id = ci.country_id;
+                INNER JOIN country co ON co.country_id = ci.country_id
+            WHERE
+                (first_name LIKE ? OR last_name LIKE ?) AND
+                (active LIKE ?)
         """);
     }
 
+    public void setSearchName(String searchAddress) {
+        this.searchName = searchAddress;
+    }
+    public void setSearchActive(Boolean searchActive) {
+        this.searchActive = searchActive;
+    }
 
     public ResultSet getAll() throws SQLException {
+        String name = searchName == null ? "%" : "%" + searchName + "%";
+        String active = searchActive == null ? "%" : "%" + (searchActive ? 1 : 0) + "%";
+        selectAll.setString(1, name);
+        selectAll.setString(2, name);
+        selectAll.setString(3, active);
+        System.out.println(active);
+
         return selectAll.executeQuery();
     }
 }
