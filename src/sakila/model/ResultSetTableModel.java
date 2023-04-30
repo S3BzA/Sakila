8df9a650948f5175e6ceeff9c0739ecfa1ac1737
vafdaf12 <vafdaf11@gmail.com>
2023-04-30 13:42:23 +0200

    feat: Support result set updating and custom column names

--- src/sakila/model/ResultSetTableModel.java
+++ src/sakila/model/ResultSetTableModel.java
@@ -2,6 +2,7 @@ package sakila.model;
 
 import javax.swing.table.AbstractTableModel;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class ResultSetTableModel extends AbstractTableModel {
@@ -10,16 +11,53 @@ public class ResultSetTableModel extends AbstractTableModel {
     String[] columnNames;
 
     public ResultSetTableModel(ResultSet set) throws SQLException {
+        rowCount = 0;
+        colCount = 0;
+        columnNames = new String[0];
+        setResultSet(set);
+    }
+
+    public ResultSetTableModel() {
+        rowCount = 0;
+        colCount = 0;
+        columnNames = new String[0];
+        resultSet = null;
+    }
+
+    public void setResultSet(ResultSet set) throws SQLException {
         this.resultSet = set;
-
-        set.last();
-        this.rowCount = set.getRow();
-        this.colCount = set.getMetaData().getColumnCount();
-
-        columnNames = new String[colCount];
-        for(int i = 0; i < colCount; i++) {
-            columnNames[i] = set.getMetaData().getColumnName(i+1);
+        if(set == null) {
+            rowCount = 0;
+            colCount = 0;
+            columnNames = new String[0];
+            return;
         }
+
+        ResultSetMetaData meta = set.getMetaData();
+        set.last();
+
+        this.rowCount = set.getRow();
+        this.colCount = meta.getColumnCount();
+
+        boolean structureChanged = colCount != columnNames.length;
+        String[] cols = new String[colCount];
+        for(int i = 0; i < colCount; i++) {
+            cols[i] = set.getMetaData().getColumnName(i+1);
+            if(structureChanged) continue;
+            if(!cols[i].equals(columnNames[i])) structureChanged = true;
+        }
+        this.columnNames = cols;
+        if(structureChanged) fireTableStructureChanged();
+
+        fireTableDataChanged();
+    }
+    public void setColumns(String[] columns) throws IllegalArgumentException {
+        if(columns.length != columnNames.length)
+            throw new IllegalArgumentException("Invalid number of columns (excepted " + columnNames.length + ")");
+
+        columnNames = columns.clone();
+        colCount = columns.length;
+        fireTableStructureChanged();
     }
 
     @Override
