aaa02c7e065e36ca31837d414fa802a4e3afb82b
S3BzA <sebguim2002@gmail.com>
2023-05-09 22:15:35 +0200

    feat: Added Film model and view
    Created the interface for films

--- src/sakila/Main.java
+++ src/sakila/Main.java
@@ -1,38 +1,27 @@
 package sakila;
 
 import sakila.model.DatabaseConfig;
+import sakila.model.StaffModel;
+import sakila.model.FilmModel;
+import sakila.view.StaffTab;
+import sakila.view.FilmTab;
 
 import javax.swing.*;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        DatabaseConfig config = DatabaseConfig.load();
-        System.out.println(config.getConnectionString());
 
-        JFrame frame = new JFrame();
-        frame.setSize(800, 600);
+        JFrame frame = new JFrame("Film");
+        JPanel panel = new FilmTab(new FilmModel());
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        System.out.println("connecting...");
-        Connection conn = DriverManager.getConnection(config.getConnectionString(), config.getUsername(), config.getPassword());
-        PreparedStatement statement = conn.prepareStatement("SELECT make, model FROM cars");
-        ResultSet set = statement.executeQuery();
-        int count = 0;
-        while(set.next()) {
-            count++;
-            String make = set.getString(1);
-            String model = set.getString(2);
-            panel.add(new JLabel(make + " " + model));
-        }
-
-        conn.close();
-        System.out.println("closed! " + count);
         frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
         frame.setVisible(true);
 
-
+//        System.out.println("closed! " + count);
+//        frame.setContentPane(panel);
+//        frame.setVisible(true);
     }
 }
