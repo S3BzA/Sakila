373bc2790aef9b32976430c11968c539d4c1ee19
vafdaf12 <vafdaf11@gmail.com>
2023-05-10 08:37:18 +0200

    fix: Film insertion error

--- src/sakila/model/FilmModel.java
+++ src/sakila/model/FilmModel.java
@@ -1,21 +1,19 @@
 package sakila.model;
 
 import java.sql.*;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.*;
 
 public class FilmModel {
 
     private final PreparedStatement filmInsert;
     private final PreparedStatement filmSelect;
-    private final PreparedStatement langInsert;
-    private static Map<String, String> inputMap = new HashMap<>();
+    private final PreparedStatement addLanguage;
+    private static final Map<String, String> inputMap = new HashMap<>();
     public FilmModel() throws SQLException//Initialises the prepared statements as well as a parameter map
     {
         Connection connection = Database.getConnection();
-        this.filmInsert = connection.prepareStatement("""
+        filmInsert = connection.prepareStatement("""
                 INSERT INTO film (
                 title,
                 description,
@@ -31,13 +29,11 @@ public class FilmModel {
                 )
                 VALUES (?,?,?,?,?,?,?,?,?,?,?)
                 """);//leaves out last_update and film_id (auto-incremented)
-        this.filmSelect = connection.prepareStatement("""
-                SELECT * FROM film
-                """);
-        this.langInsert = connection.prepareStatement("""
-                INSERT INTO language (name)
-                VALUES (?)
-                """, Statement.RETURN_GENERATED_KEYS);
+        filmSelect = connection.prepareStatement(" SELECT * FROM film");
+        addLanguage = connection.prepareStatement(
+            "INSERT INTO language(name) VALUES (?)",
+            Statement.RETURN_GENERATED_KEYS
+        );
 
 //        inputMap.put("film_id",null);
         inputMap.put("title",null);
@@ -52,6 +48,11 @@ public class FilmModel {
         inputMap.put("rating",null);
         inputMap.put("special_features",null);
 //        inputMap.put("last_update",null);
+    }
+    public int addLanguage(String language) throws SQLException {
+        addLanguage.setString(1, language);
+        addLanguage.executeUpdate();
+        return getInsertKey(addLanguage);
     }
 
     private static int getInsertKey(PreparedStatement stmt) throws SQLException
@@ -71,32 +72,24 @@ public class FilmModel {
         return inputMap.get(key);
     }
 
-    public int insertLang(String lang_name) throws SQLException
-    {
-        this.langInsert.setString(1, lang_name);
-        this.langInsert.executeUpdate();
-        return getInsertKey(langInsert);
-    }
-
     public void insertFilm(String lang1, String lang2) throws SQLException
     {
-        System.out.println("InsertFilm");
-        this.filmInsert.setString(1,inputMap.get("title"));
-        this.filmInsert.setString(2,inputMap.get("description"));
-        this.filmInsert.setInt(3, Integer.parseInt(inputMap.get("release_year")));
-        this.filmInsert.setInt(4, insertLang(lang1));
-        System.out.println("Language 1");
-        this.filmInsert.setInt(5, insertLang(lang2));
-        System.out.println("Language 2");
-        this.filmInsert.setInt(6, Integer.parseInt(inputMap.get("rental_duration")));
-        this.filmInsert.setDouble(7, Double.parseDouble(inputMap.get("rental_rate")));
-        this.filmInsert.setInt(8, Integer.parseInt(inputMap.get("length")));
-        this.filmInsert.setDouble(9, Double.parseDouble(inputMap.get("replacement_cost")));
-        this.filmInsert.setString(10, inputMap.get("rating"));
-        this.filmInsert.setString(11, inputMap.get("special_features"));
-        System.out.println("InsertFilm Executed");
-        this.filmInsert.executeUpdate();
-        System.out.println(getInsertKey(this.langInsert));
+        int id1 = addLanguage(lang1);
+        int id2 = addLanguage(lang2);
+
+        filmInsert.setString(1,inputMap.get("title"));
+        filmInsert.setString(2,inputMap.get("description"));
+        filmInsert.setInt(3, Integer.parseInt(inputMap.get("release_year")));
+        filmInsert.setInt(4, id1);
+        filmInsert.setInt(5, id2);
+        filmInsert.setInt(6, Integer.parseInt(inputMap.get("rental_duration")));
+        filmInsert.setDouble(7, Double.parseDouble(inputMap.get("rental_rate")));
+        filmInsert.setInt(8, Integer.parseInt(inputMap.get("length")));
+        filmInsert.setDouble(9, Double.parseDouble(inputMap.get("replacement_cost")));
+        filmInsert.setString(10, inputMap.get("rating"));
+        filmInsert.setString(11, inputMap.get("special_features"));
+        filmInsert.executeUpdate();
+
     }
 
     public ResultSet getResults() throws SQLException
